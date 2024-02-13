18bfb16a6ab230862b7952ff9674c1ab82fd6d63
Yury Moladau <yurymolodov@gmail.com>
2024-02-13 11:02:30 +0100

    add check operators for removeLabelFromQuery

--- src/modifyQuery.ts
+++ src/modifyQuery.ts
@@ -3,9 +3,10 @@ export function queryHasFilter(query: string, key: string, value: string): boole
 }
 
 export const removeLabelFromQuery = (query: string, key: string, value: string): string => {
+  const operators = ['AND', 'NOT']
   const parts = query.split(' ')
   const index = parts.findIndex((part) => part.includes(`${key}:${value}`))
-  const newParts = removeAtIndexAndBefore(parts, index)
+  const newParts = removeAtIndexAndBefore(parts, index, operators.includes(parts[index - 1]))
   return newParts.join(' ')
 }
 
@@ -13,15 +14,16 @@ export const addLabelToQuery = (query: string, key: string, value: string, opera
   return `${query} ${operator} ${key}:${value}`
 }
 
-const removeAtIndexAndBefore = (arr: string[], index: number): string[] => {
+const removeAtIndexAndBefore = (arr: string[], index: number, removeBefore: boolean): string[] => {
   if (index < 0 || index >= arr.length) {
     return arr;
   }
 
-  if (index === 0) {
-    arr.splice(index, 1);
+  if (removeBefore) {
+    const isStart = index === 0;
+    arr.splice(isStart ? index : index - 1, isStart ? 1 : 2);
   } else {
-    arr.splice(index - 1, 2);
+    arr.splice(index, 1);
   }
 
   return arr;
