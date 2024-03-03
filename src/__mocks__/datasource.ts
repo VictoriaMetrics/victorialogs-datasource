0bea5d55c1be52fb30ab91ead1b33e8b66125e26
Yury Moladau <yurymolodov@gmail.com>
2024-03-03 20:55:55 +0100

    test: add tests for variable replacement

---
+++ src/__mocks__/datasource.ts
@@ -0,0 +1,51 @@
+import { DataSourceInstanceSettings, PluginType } from '@grafana/data';
+import { TemplateSrv } from '@grafana/runtime';
+
+import { VictoriaLogsDatasource } from '../datasource';
+import { Options } from '../types';
+
+const defaultTemplateSrvMock = {
+  replace: (input: string) => input,
+};
+
+export function createDatasource(
+  templateSrvMock: Partial<TemplateSrv> = defaultTemplateSrvMock,
+  settings: Partial<DataSourceInstanceSettings<Options>> = {}
+): VictoriaLogsDatasource {
+  const customSettings: DataSourceInstanceSettings<Options> = {
+    url: 'myloggingurl',
+    id: 0,
+    uid: '',
+    type: '',
+    name: '',
+    meta: {
+      id: 'id',
+      name: 'name',
+      type: PluginType.datasource,
+      module: '',
+      baseUrl: '',
+      info: {
+        author: {
+          name: 'Test',
+        },
+        description: '',
+        links: [],
+        logos: {
+          large: '',
+          small: '',
+        },
+        screenshots: [],
+        updated: '',
+        version: '',
+      },
+    },
+    readOnly: false,
+    jsonData: {
+      maxLines: '20',
+    },
+    access: 'direct',
+    ...settings,
+  };
+
+  return new VictoriaLogsDatasource(customSettings, templateSrvMock as TemplateSrv);
+}
