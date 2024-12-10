49f21ee1a667eb031534651901cb64895506965c
Yury Molodov <yurymolodov@victoriametrics.com>
2024-12-10 10:44:55 +0100

    Add support /select/logsql/hits for log histograms (#146)
    * add support `/select/logsql/hits` for log histograms
    
    * issue-142: add hits endpoint
    
    * issue-142: add hits endpoint
    
    * issue-142: add hits endpoint
    
    * issue-142: add hits endpoint
    
    * issue-142: fix labels
    
    * issue-142: add field to the query
    
    * issue-142: fix value labels
    
    * issue-142: add field config
    
    * issue-142: fix display name from ds
    
    * issue-142: add hits endpoint
    
    issue-142: add hits endpoint
    
    issue-142: add hits endpoint
    
    issue-142: add hits endpoint
    
    issue-142: fix labels
    
    issue-142: add field to the query
    
    issue-142: fix value labels
    
    issue-142: add field config
    
    issue-142: fix display name from ds
    
    issue-142: fix display hits
    
    * add step param to `/select/logsql/hits`
    
    * issue-142: add tests for hits url
    
    * issue-142: add response tests
    
    * issue-142: fix comments
    
    * issue-142: fix tests
    
    ---------
    
    Co-authored-by: dmitryk-dk <kozlovdmitriyy@gmail.com>

--- pkg/plugin/query.go
+++ pkg/plugin/query.go
@@ -21,6 +21,7 @@ const (
 	tailQueryPath       = "/select/logsql/tail"
 	statsQueryPath      = "/select/logsql/stats_query"
 	statsQueryRangePath = "/select/logsql/stats_query_range"
+	hitsQueryPath       = "/select/logsql/hits"
 	defaultMaxLines     = 1000
 	legendFormatAuto    = "__auto"
 	metricsName         = "__name__"
@@ -37,6 +38,8 @@ const (
 	QueryTypeStats QueryType = "stats"
 	// QueryTypeStatsRange represents stats range query type
 	QueryTypeStatsRange QueryType = "statsRange"
+	// QueryTypeHits represents hits query type
+	QueryTypeHits QueryType = "hits"
 )
 
 // Query represents backend query object
@@ -50,6 +53,7 @@ type Query struct {
 	IntervalMs   int64     `json:"intervalMs"`
 	MaxLines     int       `json:"maxLines"`
 	Step         string    `json:"step"`
+	Field        string    `json:"field"`
 	QueryType    QueryType `json:"queryType"`
 	url          *url.URL
 }
@@ -80,6 +84,12 @@ func (q *Query) getQueryURL(rawURL string, queryParams string) (string, error) {
 			return "", fmt.Errorf("failed to calculate minimal interval: %w", err)
 		}
 		return q.statsQueryRangeURL(params, minInterval), nil
+	case QueryTypeHits:
+		minInterval, err := q.calculateMinInterval()
+		if err != nil {
+			return "", fmt.Errorf("failed to calculate minimal interval: %w", err)
+		}
+		return q.histQueryURL(params, minInterval), nil
 	default:
 		return q.queryInstantURL(params), nil
 	}
@@ -210,6 +220,42 @@ func (q *Query) statsQueryRangeURL(queryParams url.Values, minInterval time.Dura
 	values.Set("start", strconv.FormatInt(q.TimeRange.From.Unix(), 10))
 	values.Set("end", strconv.FormatInt(q.TimeRange.To.Unix(), 10))
 	values.Set("step", step)
+
+	q.url.RawQuery = values.Encode()
+	return q.url.String()
+}
+
+// histQueryURL prepare query url for querying log hits
+func (q *Query) histQueryURL(queryParams url.Values, minInterval time.Duration) string {
+	q.url.Path = path.Join(q.url.Path, hitsQueryPath)
+	values := q.url.Query()
+
+	for k, vl := range queryParams {
+		for _, v := range vl {
+			values.Add(k, v)
+		}
+	}
+
+	now := time.Now()
+	if q.TimeRange.From.IsZero() {
+		q.TimeRange.From = now.Add(-time.Minute * 5)
+	}
+	if q.TimeRange.To.IsZero() {
+		q.TimeRange.To = now
+	}
+
+	q.Expr = utils.ReplaceTemplateVariable(q.Expr, q.IntervalMs, q.TimeRange)
+
+	step := q.Step
+	if step == "" {
+		step = utils.CalculateStep(minInterval, q.TimeRange, q.MaxDataPoints).String()
+	}
+
+	values.Set("query", q.Expr)
+	values.Set("start", strconv.FormatInt(q.TimeRange.From.Unix(), 10))
+	values.Set("end", strconv.FormatInt(q.TimeRange.To.Unix(), 10))
+	values.Set("step", step)
+	values.Set("field", q.Field)
 
 	q.url.RawQuery = values.Encode()
 	return q.url.String()
--- pkg/plugin/response.go
+++ pkg/plugin/response.go
@@ -28,6 +28,7 @@ const (
 	gLabelsField = "labels"
 	gTimeField   = "Time"
 	gLineField   = "Line"
+	gValueField  = "Value"
 
 	logsVisualisation = "logs"
 )
@@ -288,6 +289,22 @@ func parseStatsResponse(reader io.Reader, q *Query) backend.DataResponse {
 	return backend.DataResponse{Frames: frames}
 }
 
+func parseHitsResponse(reader io.Reader) backend.DataResponse {
+	var hr HitsResponse
+	if err := json.NewDecoder(reader).Decode(&hr); err != nil {
+		err = fmt.Errorf("failed to decode body response: %w", err)
+		return newResponseError(err, backend.StatusInternal)
+	}
+
+	frames, err := hr.getDataFrames()
+	if err != nil {
+		err = fmt.Errorf("failed to prepare data from response: %w", err)
+		return newResponseError(err, backend.StatusInternal)
+	}
+
+	return backend.DataResponse{Frames: frames}
+}
+
 // parseErrorResponse reads data from the reader and returns error
 func parseErrorResponse(reader io.Reader) error {
 	var rs Response
@@ -418,4 +435,58 @@ func (r *Response) getDataFrames() (data.Frames, error) {
 	default:
 		return nil, fmt.Errorf("unknown result type %q", r.Data.ResultType)
 	}
+}
+
+// Hit represents a single hit from the query
+type Hit struct {
+	Fields     map[string]string `json:"fields"`
+	Timestamps []string          `json:"timestamps"`
+	Values     []float64         `json:"values"`
+	Total      int               `json:"total"`
+}
+
+// HitsResponse represents response from the hits query
+type HitsResponse struct {
+	Hits []Hit `json:"hits"`
+}
+
+func (hr *HitsResponse) getDataFrames() (data.Frames, error) {
+	frames := make(data.Frames, len(hr.Hits))
+	for i, hit := range hr.Hits {
+		if len(hit.Timestamps) != len(hit.Values) {
+			return nil, fmt.Errorf("timestamps and values length mismatch: %d != %d", len(hit.Timestamps), len(hit.Values))
+		}
+
+		timeFd := data.NewFieldFromFieldType(data.FieldTypeTime, len(hit.Timestamps))
+		timeFd.Name = gTimeField
+
+		valueFd := data.NewFieldFromFieldType(data.FieldTypeFloat64, len(hit.Values))
+		valueFd.Name = gValueField
+		valueFd.Labels = make(data.Labels)
+
+		for j, ts := range hit.Timestamps {
+			getTime, err := utils.GetTime(ts)
+			if err != nil {
+				return nil, fmt.Errorf("error parse time from _time field: %s", err)
+			}
+			timeFd.Set(j, getTime)
+		}
+
+		for k, v := range hit.Values {
+			valueFd.Set(k, v)
+		}
+
+		for key, value := range hit.Fields {
+			valueFd.Labels[key] = value
+			d, err := labelsToJSON(valueFd.Labels)
+			if err != nil {
+				return nil, fmt.Errorf("error convert labels to json: %s", err)
+			}
+			valueFd.Config = &data.FieldConfig{DisplayNameFromDS: string(d)}
+		}
+
+		frames[i] = data.NewFrame("", timeFd, valueFd)
+	}
+
+	return frames, nil
 }
