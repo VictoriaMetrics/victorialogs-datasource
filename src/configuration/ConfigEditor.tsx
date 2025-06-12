944c05458a3424b76fd6a056c3f0f4f960884013
Yury Molodov <yurymolodov@gmail.com>
2025-06-12 16:22:15 +0200

    refactor: unify props type for config editors

--- src/configuration/ConfigEditor.tsx
+++ src/configuration/ConfigEditor.tsx
@@ -15,7 +15,7 @@ import { LogLevelRulesEditor } from "./LogLevelRules/LogLevelRulesEditor";
 import { LogsSettings } from './LogsSettings';
 import { QuerySettings } from './QuerySettings';
 
-export type Props = DataSourcePluginOptionsEditorProps<Options>;
+export type PropsConfigEditor = DataSourcePluginOptionsEditorProps<Options>;
 
 const makeJsonUpdater = <T extends any>(field: keyof Options) =>
   (options: DataSourceSettings<Options>, value: T): DataSourceSettings<Options> => ({
@@ -29,7 +29,7 @@ const makeJsonUpdater = <T extends any>(field: keyof Options) =>
 const setMaxLines = makeJsonUpdater('maxLines');
 const setDerivedFields = makeJsonUpdater('derivedFields');
 
-const ConfigEditor = (props: Props) => {
+const ConfigEditor = (props: PropsConfigEditor) => {
   const { options, onOptionsChange } = props;
 
   return (
@@ -41,9 +41,9 @@ const ConfigEditor = (props: Props) => {
         onChange={onOptionsChange}
         sigV4AuthToggleEnabled={config.sigV4AuthEnabled}
       />
-      <AlertingSettings options={options} onOptionsChange={onOptionsChange}/>
+      <AlertingSettings {...props}/>
 
-      <Space v={5} />
+      <Space v={5}/>
 
       <LimitsSettings {...props}>
         <QuerySettings
