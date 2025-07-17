e0cb19da086babcdcd3be66ddf9f611a65b091d4
dmitryk-dk <kozlovdmitriyy@gmail.com>
2025-07-17 17:53:38 +0200

    cleanup

--- pkg/utils/stream_fields_parser_test.go
+++ pkg/utils/stream_fields_parser_test.go
@@ -195,7 +195,7 @@ func TestParseStreamFields(t *testing.T) {
 			wantErr:      false,
 		},
 		{
-			name:         "complex stream fields",
+			name:         "complex stream fields with the comma inside the value",
 			streamFields: `{a="b", c="d,e,f", g="h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"}`,
 			want: []StreamField{
 				{
