20c7c4a30df73a29a97efba49b0b8420838bfdb1
Yury Molodov <yurymolodov@victoriametrics.com>
2025-09-08 15:40:30 +0200

    feat: Ad hoc filters support multi-value operators (#293) (#371)

--- src/modifyQuery.test.ts
+++ src/modifyQuery.test.ts
@@ -7,7 +7,7 @@ describe('modifyQuery', () => {
       const key = 'baz';
       const value = 'qux';
       const operator = '=';
-      const result = addLabelToQuery(query, key, value, operator);
+      const result = addLabelToQuery(query, { key, value, operator });
       expect(result).toBe('foo: bar AND baz:="qux"');
     });
 
@@ -16,7 +16,7 @@ describe('modifyQuery', () => {
       const key = 'baz';
       const value = 'qux';
       const operator = '=';
-      const result = addLabelToQuery(query, key, value, operator);
+      const result = addLabelToQuery(query, { key, value, operator });
       expect(result).toBe('foo: bar AND baz:="qux" | pipe1 | pipe2');
     });
 
@@ -24,16 +24,28 @@ describe('modifyQuery', () => {
       const query = 'foo: bar | pipe1 | pipe2';
       const key = '_stream';
       const value = '{event: "test"}';
-      expect(addLabelToQuery(query, key, value, '=')).toBe('foo: bar AND _stream:{event: "test"} | pipe1 | pipe2');
-      expect(addLabelToQuery(query, key, value, '!=')).toBe('foo: bar AND (! _stream: {event: "test"}) | pipe1 | pipe2');
+      expect(addLabelToQuery(query, { key, value, operator: '=' })).toBe('foo: bar AND _stream:{event: "test"} | pipe1 | pipe2');
+      expect(addLabelToQuery(query, { key, value, operator: '!=' })).toBe('foo: bar AND (! _stream: {event: "test"}) | pipe1 | pipe2');
     });
 
     it('should add ":" "!:" for _stream_id key', () => {
       const query = 'foo: bar | pipe1 | pipe2';
       const key = '_stream_id';
       const value = 'stream123';
-      expect(addLabelToQuery(query, key, value, '=')).toBe('foo: bar AND _stream_id:stream123 | pipe1 | pipe2');
-      expect(addLabelToQuery(query, key, value, '!=')).toBe('foo: bar AND (! _stream_id: stream123) | pipe1 | pipe2');
+      expect(addLabelToQuery(query, { key, value, operator: '=' })).toBe('foo: bar AND _stream_id:stream123 | pipe1 | pipe2');
+      expect(addLabelToQuery(query, { key, value, operator: '!=' })).toBe('foo: bar AND (! _stream_id: stream123) | pipe1 | pipe2');
+    });
+
+    it('should add "=|" group', () => {
+      const query = 'foo: bar';
+      const result = addLabelToQuery(query, { key: 'baz', value: '', values: ['qux', 'quux'], operator: '=|' });
+      expect(result).toBe('foo: bar AND baz:in("qux","quux")');
+    });
+
+    it('should add "!=|" group', () => {
+      const query = 'foo: bar';
+      const result = addLabelToQuery(query, { key: 'baz', value: '', values: ['qux', 'quux'], operator: '!=|' });
+      expect(result).toBe('foo: bar AND !(baz:in("qux","quux"))');
     });
   });
 
