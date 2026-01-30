dd42bf11cf06589ac0a1fb7cae6244df58e4dd0b
Artur Minchukov <aminchukov@victoriametrics.com>
2026-01-30 13:39:24 +0400

    add `histogram` and `json_values` stats pipe to statsPipeFunctions func

---
+++ src/LogsQL/statsPipeFunctions.test.ts
@@ -0,0 +1,13 @@
+import { isExprHasStatsPipeFunctions, statsPipeFunctions } from "./statsPipeFunctions";
+
+describe("isExprHasStatsPipeFunctions", () => {
+  describe("should return true for each stats pipe function", () => {
+    it.each(statsPipeFunctions)("detects '%s' function", (func) => {
+      expect(isExprHasStatsPipeFunctions(`* | stats by () ${func}()`)).toBe(true);
+    });
+
+    it.each(statsPipeFunctions)("detects '%s' function in upper case", (func) => {
+      expect(isExprHasStatsPipeFunctions(`* | stats by () ${func.toUpperCase()}()`)).toBe(true);
+    });
+  });
+});
